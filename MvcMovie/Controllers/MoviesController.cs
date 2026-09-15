b928f853d20f0aa111e1a51d3394abd9bd59a56e
elsilaves <admin@gilver.hu>
2026-09-15 23:05:10 +0200

    Controler: searchString, LINQ query Lambda Expression

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -13,9 +13,26 @@ public class MoviesController : Controller
     }
 
     // GET: MOVIES
-    public async Task<IActionResult> Index()    
+    public async Task<IActionResult> Index(string searchString)    
     {
-        return View(await _context.Movie.ToListAsync());
+        if (_context.Movie == null)
+        {
+            return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        }
+
+        // The following line in the Index action method creates a LINQ query to select the movies
+        var movies = from m in _context.Movie
+                     select m;
+
+        //If the searchString parameter contains a string, the movies query is modified to filter on the value of the search string:
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(
+                // Lambda Expression. Lambdas are used in method-based LINQ queries as arguments to standard query operator methods such as the Where method or Contains.
+                s => s.Title!.ToUpper().Contains(searchString.ToUpper())); 
+        }
+        //return View(await _context.Movie.ToListAsync());
+        return View(await movies.ToListAsync());
     }
 
     // GET: MOVIES/Details/5
